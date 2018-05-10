9d1d84de5fa9765abfbf736659daeb89c2a634af
nicolepristin <nicole.pristin@gmail.com>
2018-05-09 21:16:42 -0400

    Add method for finding a user by id

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -2,6 +2,10 @@ package webdev.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import webdev.models.User;
+
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    List<User> findById(int id);
 }

30816a5cac5d7bf6986c02c75f5bdfd18cc73be7
nicolepristin <nicole.pristin@gmail.com>
2018-05-09 21:17:33 -0400

    Add user service for handling api

--- src/main/java/webdev/services/UserService.java
+++ src/main/java/webdev/services/UserService.java
@@ -1,4 +1,14 @@
 package webdev.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+import webdev.models.User;
+import webdev.repositories.UserRepository;
+
+@RestController
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
 }

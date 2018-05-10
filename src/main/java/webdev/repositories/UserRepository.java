cbbe0a2d24bb73eb17930064f39f28eb0540a8ff
nicolepristin <nicole.pristin@gmail.com>
2018-05-09 21:13:40 -0400

    Change from crud to jpa repository

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package webdev.repositories;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import webdev.models.User;
-public interface UserRepository extends CrudRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}

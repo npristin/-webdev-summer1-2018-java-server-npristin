ae9dbed0cc36d0c59b1e9469cc90b9dad165706f
nicolepristin <nicole.pristin@gmail.com>
2018-05-10 23:41:25 -0400

    Add findUserByUsername to user repository

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -1,7 +1,13 @@
 package webdev.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import webdev.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("SELECT u FROM user u WHERE u.username=:username")
+    Iterable<User> findUserByUsername(@Param("username") String u);
+
 }

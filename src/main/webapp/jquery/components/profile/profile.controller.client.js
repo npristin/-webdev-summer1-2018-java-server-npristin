482ca449fe80c539acb8421df15699abcda7f292
nicolepristin <nicole.pristin@gmail.com>
2018-05-13 13:56:33 -0400

    Set up boilerplate code in profile controller

---
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -0,0 +1,18 @@
+(function () {
+    var $usernameFld, $phoneFld, $emailFld, $roleFld, $dateOfBirthFld;
+    var $updateBtn, $logoutBtn;
+    var userService = new UserServiceClient();
+    $(main);
+
+    function main() {
+    
+    }
+
+    function updateProfile() {
+
+    }
+
+    function logout() {
+
+    }
+})();

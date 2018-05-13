11311904d52e1ca532783dc5f3d3e65f432f4975
nicolepristin <nicole.pristin@gmail.com>
2018-05-13 12:16:57 -0400

    Implement login method in login controller

--- src/main/webapp/jquery/components/login/login.controller.client.js
+++ src/main/webapp/jquery/components/login/login.controller.client.js
@@ -5,10 +5,29 @@
     $(main);
 
     function main() {
-
+        $loginBtn = $('.btn-block');
+        $loginBtn.click(login);
     }
 
     function login() {
+        console.log("logging in!");
 
+        $usernameFld = $('#username').val();
+        $passwordFld = $('#password').val();
+
+        var user = new User();
+        user.setUsername($usernameFld);
+        user.setPassword($passwordFld);
+
+        userService.login(user)
+            .then(function (response) {
+                console.log(response);
+                if (response.ok) {
+                    window.location.href =
+                        'http://localhost:8080/jquery/components/register/register.template.client.html';
+                } else {
+                    alert('Invalid username and password!');
+                }
+        });
     }
 })();

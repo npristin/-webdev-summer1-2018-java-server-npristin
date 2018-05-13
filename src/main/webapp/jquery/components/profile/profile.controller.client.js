8921a32f668dcfe1dd5c0a8aa6e93429bada4c7c
nicolepristin <nicole.pristin@gmail.com>
2018-05-13 17:00:11 -0400

    Write profile methods to populate the profile page

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -5,14 +5,41 @@
     $(main);
 
     function main() {
-    
+        $updateBtn = $('#update');
+        $logoutBtn = $('#logout');
+
+        $updateBtn.click(updateProfile);
+        $logoutBtn.click(logout);
+
+        retrieveUserData();
     }
 
-    function updateProfile() {
+    function retrieveUserData() {
+        // parses query parameters to get userId, these 3 lines of code are used in reference from:
+        // https://forums.asp.net/t/1903621.aspx?how+can+I+get+two+parameters+values+from+Query+string+using+jquery+
+        var queries = {};
+        $.each(document.location.search.substr(1).split('&'),function(c,q){ var i = q.split('=');
+        queries[i[0].toString()] = i[1].toString(); });
 
+        console.log(queries.userId);
+        userId = queries.userId;
+
+        if (typeof userId === "undefined") {
+           alert("No user selected for update");
+        } else {
+            userService.findUserById(userId).then(populateProfile);
+        }
     }
 
-    function logout() {
+    function populateProfile(userResponse) {
+        console.log("populating user form");
 
+        $('#username').val(userResponse.username);
+        $("#username").prop("readonly", true);
+
+        $('#phone').val(userResponse.phone);
+        $('#email').val(userResponse.email);
+        $('#role').val(userResponse.role);
+        $('#datOfBirth').val(userResponse.dateOfBirth);
     }
 })();

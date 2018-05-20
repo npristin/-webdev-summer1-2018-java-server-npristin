468cc64b60def758d98b090d9640d7815b20b8d5
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 18:07:50 -0400

    Add lesson service and relevant repositories

--- src/main/java/webdev/services/LessonService.java
+++ src/main/java/webdev/services/LessonService.java
@@ -1,4 +1,22 @@
 package webdev.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import webdev.models.Lesson;
+import webdev.models.Module;
+import webdev.repositories.LessonRepository;
+import webdev.repositories.ModuleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class LessonService {
+
+    @Autowired
+    LessonRepository lessonRepository;
+    @Autowired
+    ModuleRepository moduleRepository;
+
 }

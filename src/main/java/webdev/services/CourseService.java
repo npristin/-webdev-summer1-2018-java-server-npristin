24feaad89da0ca2af7fbdb9212cd5c3b4e9d973c
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 18:02:52 -0400

    Add a course service and a create course method

--- src/main/java/webdev/services/CourseService.java
+++ src/main/java/webdev/services/CourseService.java
@@ -1,4 +1,29 @@
 package webdev.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import webdev.models.Course;
+import webdev.repositories.CourseRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CourseService {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @PostMapping("/api/course")
+    public Course createCourse(@RequestBody Course course) {
+        Date date = new Date();
+        course.setCreated(date);
+        course.setModified(date);
+        courseRepository.save(course);
+        return course;
+    }
+
+    
 }

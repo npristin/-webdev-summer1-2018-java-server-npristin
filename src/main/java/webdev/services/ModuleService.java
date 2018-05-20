c30d4827bb10a864b78f676d4de9896f4cc00c19
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 18:04:47 -0400

    Add module service and module/course repo

--- src/main/java/webdev/services/ModuleService.java
+++ src/main/java/webdev/services/ModuleService.java
@@ -1,4 +1,22 @@
 package webdev.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import webdev.models.Course;
+import webdev.models.Module;
+import webdev.repositories.CourseRepository;
+import webdev.repositories.ModuleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ModuleService {
+
+    @Autowired
+    ModuleRepository moduleRepository;
+    @Autowired
+    CourseRepository courseRepository;
+
 }

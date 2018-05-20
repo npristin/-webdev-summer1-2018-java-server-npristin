cd0940ac8716d891656989f947d86de2194fe2ad
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 18:06:45 -0400

    Add a lesson data object

--- src/main/java/webdev/models/Lesson.java
+++ src/main/java/webdev/models/Lesson.java
@@ -1,4 +1,50 @@
 package webdev.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity(name="lesson")
 public class Lesson {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    @ManyToOne
+    @JsonIgnore
+    private Module module;
+
+    public Lesson(int id, String title, Module module) {
+        this.id = id;
+        this.title = title;
+        this.module = module;
+    }
+
+    public Lesson() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Module getModule() {
+        return module;
+    }
+
+    public void setModule(Module module) {
+        this.module = module;
+    }
 }

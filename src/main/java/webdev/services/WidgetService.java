bc13e3cc89ceb95bef88a7cfb144638c07c60645
nicolepristin <nicole.pristin@gmail.com>
2018-05-25 19:52:19 -0400

    Add findAllWidgets endpoint to service

--- src/main/java/webdev/services/WidgetService.java
+++ src/main/java/webdev/services/WidgetService.java
@@ -2,8 +2,16 @@ package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import webdev.models.Lesson;
+import webdev.models.Widget;
+import webdev.repositories.LessonRepository;
 import webdev.repositories.WidgetRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -12,5 +20,9 @@ public class WidgetService {
     @Autowired
     WidgetRepository widgetRepository;
 
-    
+    @GetMapping("/api/widget")
+    public List<Widget> findAllWidgets() {
+        return (List<Widget>) widgetRepository.findAll();
+    }
+
 }

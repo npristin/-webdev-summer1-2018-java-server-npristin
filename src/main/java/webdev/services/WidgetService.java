bc83f2cbe1e5ab0985094eb78784cb4e78c248ac
nicolepristin <nicole.pristin@gmail.com>
2018-05-25 19:44:28 -0400

    Add widget repository to widget service

--- src/main/java/webdev/services/WidgetService.java
+++ src/main/java/webdev/services/WidgetService.java
@@ -1,4 +1,16 @@
 package webdev.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import webdev.repositories.WidgetRepository;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WidgetService {
+
+    @Autowired
+    WidgetRepository widgetRepository;
+
+    
 }

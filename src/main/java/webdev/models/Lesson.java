1ee2cb3045edcb6b7d7539950eed621eeaf305b0
nicolepristin <nicole.pristin@gmail.com>
2018-05-25 19:53:25 -0400

    Add a one to many relationship in lesson to widget

--- src/main/java/webdev/models/Lesson.java
+++ src/main/java/webdev/models/Lesson.java
@@ -3,6 +3,7 @@ package webdev.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity(name="lesson")
 public class Lesson {
@@ -13,11 +14,14 @@ public class Lesson {
     @ManyToOne
     @JsonIgnore
     private Module module;
+    @OneToMany(mappedBy="lesson")
+    private List<Widget> widgets;
 
-    public Lesson(int id, String title, Module module) {
+    public Lesson(int id, String title, Module module, List<Widget> widgets) {
         this.id = id;
         this.title = title;
         this.module = module;
+        this.widgets = widgets;
     }
 
     public Lesson() {
@@ -46,5 +50,13 @@ public class Lesson {
 
     public void setModule(Module module) {
         this.module = module;
+    }
+
+    public List<Widget> getWidgets() {
+        return widgets;
+    }
+
+    public void setWidgets(List<Widget> widgets) {
+        this.widgets = widgets;
     }
 }

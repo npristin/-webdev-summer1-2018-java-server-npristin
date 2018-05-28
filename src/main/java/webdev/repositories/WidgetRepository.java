92d71add6f5ee744ed683855381c938cd644af5c
nicolepristin <nicole.pristin@gmail.com>
2018-05-28 16:11:36 -0400

    Add a sql statement that retrieves the widgets in order

--- src/main/java/webdev/repositories/WidgetRepository.java
+++ src/main/java/webdev/repositories/WidgetRepository.java
@@ -1,7 +1,11 @@
 package webdev.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import webdev.models.Widget;
 
 public interface WidgetRepository extends CrudRepository<Widget, Integer> {
+
+    @Query("SELECT w FROM widget w ORDER BY w.widgetOrder")
+    Iterable<Widget> findAllOrderByWidgetOrder();
 }

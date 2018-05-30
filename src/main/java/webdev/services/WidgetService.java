396f83e92f0458cd2248a8c28b4ae90b806070f4
nicolepristin <nicole.pristin@gmail.com>
2018-05-29 22:52:25 -0400

    Add method for decrementing the order of widgets

--- src/main/java/webdev/services/WidgetService.java
+++ src/main/java/webdev/services/WidgetService.java
@@ -9,6 +9,7 @@ import webdev.repositories.WidgetRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -21,7 +22,7 @@ public class WidgetService {
 
     @GetMapping("/api/widget")
     public List<Widget> findAllWidgets() {
-        return (List<Widget>) widgetRepository.findAll();
+        return (List<Widget>) widgetRepository.findAllOrderByWidgetOrder();
     }
 
     @GetMapping("/api/widget/{widgetId}")
@@ -67,10 +68,22 @@ public class WidgetService {
             Lesson lesson = maybeLesson.get();
             lesson.setWidgets(widgets);
 
+            // get the number of widgets that have an assigned size
+            int orderedWidgets = 0;
+            for (Widget w : widgets) {
+                if (w.getWidgetOrder() > 0) {
+                    orderedWidgets += 1;
+                }
+            }
+
             for (Widget w : widgets) {
                 if (w.getLessonId() == 0) {
                     w.setLesson(lesson);
                     w.setLessonId(lessonId);
+                }
+                if (w.getWidgetOrder() == 0) {
+                    w.setWidgetOrder(orderedWidgets + 1);
+                    orderedWidgets += 1;
                 }
                 widgetRepository.save(w);
             }
@@ -107,4 +120,28 @@ public class WidgetService {
     public void deleteWidget(@PathVariable("widgetId") int widgetId) {
         widgetRepository.deleteById(widgetId);
     }
+
+    @PostMapping("/api/lesson/{lessonId}/widget/{widgetId}/order/decrement")
+    public List<Widget> decrementOrder(@RequestBody List<Widget> widgets, @PathVariable("widgetId") int widgetId,
+                               @PathVariable("lessonId") int lessonId) {
+        List<Widget> lessonWidgets = widgets.stream().filter(w -> w.getLessonId() == lessonId)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < lessonWidgets.size(); i++) {
+            Widget w = lessonWidgets.get(i);
+            if (w.getId() == widgetId && i > 0) {
+                // replace the order of the two widgets
+                int originalOrder = w.getWidgetOrder();
+                Widget priorWidget = lessonWidgets.get(i - 1);
+                w.setWidgetOrder(priorWidget.getWidgetOrder());
+                priorWidget.setWidgetOrder(originalOrder);
+
+                widgetRepository.save(w);
+                widgetRepository.save(priorWidget);
+                break;
+            }
+        }
+        return (List<Widget>) widgetRepository.findAllOrderByWidgetOrder();
+    }
+
 }

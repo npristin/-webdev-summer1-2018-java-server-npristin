a25c6913081e0b6bce8bb7259d5ce3d1e4e41822
nicolepristin <nicole.pristin@gmail.com>
2018-06-03 13:17:24 -0400

    Add method to find all exam questions

--- src/main/java/webdev/services/BaseExamQuestionService.java
+++ src/main/java/webdev/services/BaseExamQuestionService.java
@@ -1,15 +1,30 @@
 package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import webdev.models.BaseExamQuestion;
+import webdev.models.Exam;
 import webdev.repositories.BaseExamQuestionRepository;
 import webdev.repositories.EssayExamQuestionRepository;
+import webdev.repositories.ExamRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public class BaseExamQuestionService {
 
     @Autowired
     BaseExamQuestionRepository baseExamQuestionRepository;
     @Autowired
-    EssayExamQuestionRepository essayExamQuestionRepository;
+    ExamRepository examRepository;
 
-    
+    @GetMapping("/api/exam/{eid}/question")
+    public List<BaseExamQuestion> findQuestionsByExam(@PathVariable("eid") int eid) {
+        Optional<Exam> maybeExam = examRepository.findById(eid);
+        if (maybeExam.isPresent()) {
+            return maybeExam.get().getQuestions();
+        }
+        return null;
+    }
 }

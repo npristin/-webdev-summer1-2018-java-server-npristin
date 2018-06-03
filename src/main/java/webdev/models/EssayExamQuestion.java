0feb4ae1bdc0bea5e1714a3e129b2d8f44062245
nicolepristin <nicole.pristin@gmail.com>
2018-06-03 12:24:14 -0400

    Add an essay exam question data model

---
+++ src/main/java/webdev/models/EssayExamQuestion.java
@@ -0,0 +1,9 @@
+package webdev.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "JOINED_ESSAY_QUESTION")
+public class EssayExamQuestion extends BaseExamQuestion {
+}

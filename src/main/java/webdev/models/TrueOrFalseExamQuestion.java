9e42c0477d186f3e53d55322d585bd16045c44cc
nicolepristin <nicole.pristin@gmail.com>
2018-06-03 12:32:13 -0400

    Add true false question data model

---
+++ src/main/java/webdev/models/TrueOrFalseExamQuestion.java
@@ -0,0 +1,18 @@
+package webdev.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "JOINED_TRUE_OR_FALSE_QUESTION")
+public class TrueOrFalseExamQuestion extends BaseExamQuestion {
+    @Column(name = "IS_TRUE", nullable = false)
+    private Boolean isTrue;
+    public Boolean getIsTrue() {
+        return isTrue;
+    }
+    public void setIsTrue(Boolean isTrue) {
+        this.isTrue = isTrue;
+    }
+}

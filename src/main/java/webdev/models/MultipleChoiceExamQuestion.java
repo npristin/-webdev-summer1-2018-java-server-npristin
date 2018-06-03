64e94286354c84035bde4d938d4a821e5f38d00f
nicolepristin <nicole.pristin@gmail.com>
2018-06-03 12:29:57 -0400

    Create multiple choice exam question data model

---
+++ src/main/java/webdev/models/MultipleChoiceExamQuestion.java
@@ -0,0 +1,23 @@
+package webdev.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "JOINED_MULTIPLE_CHOICE_QUESTION")
+public class MultipleChoiceExamQuestion extends BaseExamQuestion {
+    private String options;
+    private int correctOption;
+    public String getOptions() {
+        return options;
+    }
+    public void setOptions(String options) {
+        this.options = options;
+    }
+    public int getCorrectOption() {
+        return correctOption;
+    }
+    public void setCorrectOption(int correctOption) {
+        this.correctOption = correctOption;
+    }
+}

d0bcd5a8798c544ba2ffd75e72d2d35eed18946e
nicolepristin <nicole.pristin@gmail.com>
2018-06-03 12:13:59 -0400

    Add exam service and relevant repositories

---
+++ src/main/java/webdev/services/ExamService.java
@@ -0,0 +1,33 @@
+package webdev.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import webdev.models.Exam;
+import webdev.models.Lesson;
+import webdev.models.MultipleChoiceExamQuestion;
+import webdev.models.BaseExamQuestion;
+import webdev.models.TrueOrFalseExamQuestion;
+import webdev.repositories.ExamRepository;
+import webdev.repositories.LessonRepository;
+import webdev.repositories.MultipleChoiceExamQuestionRepository;
+import webdev.repositories.TrueOrFalseExamQuestionRepository;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class ExamService {
+
+    @Autowired
+    ExamRepository examRepository;
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @GetMapping("/api/exam")
+    public List<Exam> findAllExams() {
+        return (List<Exam>) examRepository.findAll();
+    }
+
+}

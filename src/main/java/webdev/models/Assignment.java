24b267e6a345f4842dde354c2eccb418358edd6a
nicolepristin <nicole.pristin@gmail.com>
2018-06-05 00:17:54 -0400

    Add assignment data model

---
+++ src/main/java/webdev/models/Assignment.java
@@ -0,0 +1,34 @@
+package webdev.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Assignment extends Widget {
+    private String title;
+    private String description;
+    private int points;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}

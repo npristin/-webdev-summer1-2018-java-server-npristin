a9ce4e0bbfa4d521bceef93ab1b911ded9bc4c83
nicolepristin <nicole.pristin@gmail.com>
2018-06-05 00:29:50 -0400

    Add method to find assignment by id

--- src/main/java/webdev/services/AssignmentService.java
+++ src/main/java/webdev/services/AssignmentService.java
@@ -3,13 +3,16 @@ package webdev.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import webdev.models.Assignment;
+import webdev.models.Lesson;
 import webdev.repositories.AssignmentRepository;
 import webdev.repositories.ExamRepository;
 import webdev.repositories.LessonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -24,4 +27,15 @@ public class AssignmentService {
     public List<Assignment> findAllAssignments() {
         return (List<Assignment>) assignmentRepository.findAll();
     }
+
+    @GetMapping("/api/assignment/{aid}")
+    public Assignment findAssignmentByid(@PathVariable("aid") int aid) {
+        Optional<Assignment> maybeAssignment = assignmentRepository.findById(aid);
+        if (maybeAssignment.isPresent()) {
+            return maybeAssignment.get();
+        }
+        return null;
+    }
+
+
 }

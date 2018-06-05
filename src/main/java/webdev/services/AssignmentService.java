7e67e8044f4bde35e252b9cf9760df53bbd60e59
nicolepristin <nicole.pristin@gmail.com>
2018-06-05 00:24:40 -0400

    Add method for finding all assignments

--- src/main/java/webdev/services/AssignmentService.java
+++ src/main/java/webdev/services/AssignmentService.java
@@ -2,16 +2,26 @@ package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import webdev.models.Assignment;
+import webdev.repositories.AssignmentRepository;
 import webdev.repositories.ExamRepository;
 import webdev.repositories.LessonRepository;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class AssignmentService {
 
     @Autowired
-    ExamRepository examRepository;
+    AssignmentRepository assignmentRepository;
     @Autowired
     LessonRepository lessonRepository;
+
+    @GetMapping("/api/assignment")
+    public List<Assignment> findAllAssignments() {
+        return (List<Assignment>) assignmentRepository.findAll();
+    }
 }
